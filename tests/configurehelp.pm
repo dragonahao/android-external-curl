83d50a8bd159dd600a46bc7edb9d2fd3923538c1
Chevy Lin <chevylin0802@gmail.com>
2014-03-28 19:17:29 +0800

    android curl library with jni interface

---
+++ tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = '/usr/local/android/kbe-rk3188-android/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-gcc -E -isystem /usr/local/android/ndk-r9c/platforms/android-18/arch-arm/usr/include -isystem /usr/local/android/kbe-rk3188-android/external/curl/include/ -isystem /usr/local/android/kbe-rk3188-android/external/openssl/include -isystem /usr/local/android/kbe-rk3188-android/external/curl -isystem /usr/local/android/kbe-rk3188-android/out/target/product/rk31sdk/obj/STATIC_LIBRARIES/libcurl_intermediates -isystem /usr/local/android/kbe-rk3188-android/dalvik/libnativehelper/include/nativehelper -isystem /usr/local/android/kbe-rk3188-android/system/core/include -isystem /usr/local/android/kbe-rk3188-android/hardware/libhardware/include -isystem /usr/local/android/kbe-rk3188-android/hardware/libhardware_legacy/include -isystem /usr/local/android/kbe-rk3188-android/hardware/ril/include -isystem /usr/local/android/kbe-rk3188-android/dalvik/libnativehelper/include -isystem /usr/local/android/kbe-rk3188-android/frameworks/base/include -isystem /usr/local/android/kbe-rk3188-android/frameworks/base/opengl/include -isystem /usr/local/android/kbe-rk3188-android/frameworks/base/native/include -isystem /usr/local/android/kbe-rk3188-android/external/skia/include -isystem /usr/local/android/kbe-rk3188-android/out/target/product/rk31sdk/obj/include -isystem /usr/local/android/kbe-rk3188-android/bionic/libc/arch-arm/include -isystem /usr/local/android/kbe-rk3188-android/bionic/libc/include -isystem /usr/local/android/kbe-rk3188-android/bionic/libstdc++/include -isystem /usr/local/android/kbe-rk3188-android/bionic/libc/kernel/common -isystem /usr/local/android/kbe-rk3188-android/bionic/libc/kernel/arch-arm -isystem /usr/local/android/kbe-rk3188-android/bionic/libm/include -isystem /usr/local/android/kbe-rk3188-android/bionic/libm/include/arch/arm -isystem /usr/local/android/kbe-rk3188-android/bionic/libthread_db/include -include /usr/local/android/kbe-rk3188-android/build/core/combo/include/arch/linux-arm/AndroidConfig.h -isystem /usr/local/android/kbe-rk3188-android/system/core/include/arch/linux-arm/ -D__ARM_ARCH_7__ -DANDROID -DNDEBUG -DHAVE_CONFIG_H';
+
+1;
